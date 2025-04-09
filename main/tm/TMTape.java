0f3f93140bed569ecf1b4bede95ee6fc45925733
Jayce Lowry <jaycelowry@u.boisestate.edu>
2025-04-08 23:09:43 -0600

    Add TMTape Interface

--- main/tm/TMTape.java
+++ main/tm/TMTape.java
@@ -1,8 +1,49 @@
 package tm;
 
 /**
- * TODO Docs
+ * An implementation of the TMTapeInterface.
+ *
+ * @author
  */
-public class TMTape {
+public class TMTape implements TMTapeInterface {
 
+    /**
+     * Creates a new TMTape with a given string.
+     *
+     * @param inputString the string on this TMTape.
+     */
+    public TMTape(String inputString) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public char read() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void write(char symbol) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void move(TMInterface.Direction direction) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getVisited() {
+        return "";
+    }
 }
