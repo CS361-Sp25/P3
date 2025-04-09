ce71ee0e722496601378404ca0f9174c440d7472
Jayce Lowry <jaycelowry@u.boisestate.edu>
2025-04-08 23:57:45 -0600

    Add basic driver class logic

--- main/tm/TMSimulator.java
+++ main/tm/TMSimulator.java
@@ -1,12 +1,72 @@
 package tm;
 
+import java.io.File;
+
 /**
- * TODO Docs
+ * Simulates a deterministic Turing Machine, constructed
+ * from an encoded file.
  *
- * @author
+ * @author Jayce Lowry
  */
 public class TMSimulator {
-    public static void main(String[] args) {
+    private final String PROGRAM_NAME = this.getClass().getName();
+    private final String USAGE = "Usage: java " + PROGRAM_NAME + " <filename>\n" +
+            "       <filename>: The name of a file containing\n" +
+            "                   an encoded Turing Machine";
 
+    private File inputFile;
+    private TM machine;
+    private String input;
+
+    /**
+     * Creates a new TNSimulator.
+     *
+     * @param args command-line arguments
+     */
+    public TMSimulator(String[] args) {
+        if (!parseArgs(args)) {
+            System.err.println(USAGE);
+            System.exit(1);
+        }
+        // TODO Exception handling
+        TMParser parser = new TMParser(inputFile);
+        machine = parser.getMachine();
+        input = parser.getInputString();
+    }
+
+    /**
+     * Runs the Turing Machine simulation and
+     * outputs results to stdout.
+     */
+    public void runSimulation() {
+        String results = machine.run(input);
+        System.out.println(results);
+    }
+
+    /**
+     * Parses command-line arguments.
+     *
+     * @param args command-line arguments.
+     * @return true if parsing succeeded,
+     * false otherwise.
+     */
+    private boolean parseArgs(String[] args) {
+        if (args.length != 1) {
+            return false;
+        }
+        boolean retVal = true;
+        String filename = args[0];
+
+        inputFile = new File(filename);
+        if (!inputFile.exists() || !inputFile.canRead() || inputFile.isDirectory()) {
+            retVal = false;
+            System.err.println("File \"" + filename + "\" does not exist or is inaccessible");
+        }
+        return retVal;
+    }
+
+    public static void main(String[] args) {
+        TMSimulator simulator = new TMSimulator(args);
+        simulator.runSimulation();
     }
 }
