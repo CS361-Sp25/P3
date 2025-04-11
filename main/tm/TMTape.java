6312f6b62ec6af1c61dfa6392f58cb4239f42a49
cstombaugh <chasestombaugh@u.boisestate.edu>
2025-04-10 23:33:13 -0600

    started TMTape

--- main/tm/TMTape.java
+++ main/tm/TMTape.java
@@ -1,11 +1,22 @@
 package tm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * An implementation of the TMTapeInterface.
+ * An implementation of the TMTapeInterface for a bi-infinite tape. This
+ * tape can read and write characters and move the head left and right 
+ * indefinitely in both directions. The tape is initialized with a string
+ * input starting at index 0. Unvisited cells default to the blank symbol 0.
  *
- * @author
+ * @author Jayce Lowry and Chase Stombaugh
  */
 public class TMTape implements TMTapeInterface {
+    
+    private final Map<Integer, Character> tape; //stores the tape values
+    private int head;
+    private int minVisited;
+    private int maxVisited;
 
     /**
      * Creates a new TMTape with a given string.
@@ -13,7 +24,16 @@ public class TMTape implements TMTapeInterface {
      * @param inputString the string on this TMTape.
      */
     public TMTape(String inputString) {
+        tape = new HashMap<>();
+        head = 0;
+        minVisited = 0;
+        maxVisited = 0;
 
+        // populating the tape with the given input string
+        for (int i = 0; i < inputString.length(); i++) {
+            tape.put(i, inputString.charAt(i));
+            maxVisited = i;
+        }
     }
 
     /**
@@ -21,7 +41,7 @@ public class TMTape implements TMTapeInterface {
      */
     @Override
     public char read() {
-        return 0;
+        return tape.getOrDefault(head, '0');
     }
 
     /**
@@ -29,7 +49,8 @@ public class TMTape implements TMTapeInterface {
      */
     @Override
     public void write(char symbol) {
-
+        tape.put(head, symbol);
+        updateBounds();
     }
 
     /**
@@ -37,13 +58,34 @@ public class TMTape implements TMTapeInterface {
      */
     @Override
     public void move(TMInterface.Direction direction) {
-
+        if (direction == TMInterface.Direction.LEFT) {
+            head--;
+        } else { 
+            head++;
+        } 
+        updateBounds();
     }
 
     /**
      * {@inheritDoc}
      */
     public String getVisited() {
-        return "";
+        StringBuilder visited = new StringBuilder();
+        for (int i = minVisited; i <= maxVisited; i++) {
+            visited.append(tape.getOrDefault(i, '0'));
+        }
+        return visited.toString();
+    }
+
+    /**
+     * Updates the bounds for the visited cells in the tape
+     */
+    private void updateBounds() {
+        if (head < minVisited) {
+            minVisited = head;
+        }
+        if (head > maxVisited) {
+            maxVisited = head;
+        }
     }
 }
